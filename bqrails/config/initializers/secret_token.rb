29108399e5350e099167e30a2da98fe7145493e7
Juan Pablo Blanco <jp.blanco@moove-it.com>
2012-03-29 17:34:47 -0300

    Fixed javascript exception

---
+++ bqrails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bqrails::Application.config.secret_token = 'b65f2f9e6eff29f22e78c9faebbfe807cd0619073b4f04f4d26fa9d3b6de739c2b25f29c2732433520979ae5db22469b94f6f845e2945355410a9b08dcb4c437'
